4a07c3c77721d5a1b3da565e8abd378c02c8cc4e
HKThanh <21086351.thanh@student.iuh.edu.vn>
2024-09-07 19:53:23 +0700

    ReactNative Screen2

--- src/Tuan03/App.js
+++ src/Tuan03/App.js
@@ -1,11 +1,13 @@
 import {View} from 'react-native';
 import React from 'react';
 import Screen1 from './Screens/Screen1';
+import Screen2 from './Screens/Screen2';
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
-      <Screen1/>
+      {/* <Screen1/> */}
+      <Screen2/>
     </View>
   );
 }
---
+++ src/Tuan03/Screens/Screen2.js
@@ -0,0 +1,54 @@
+import { Pressable, StyleSheet, Text, View, Image } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const style = StyleSheet.create({
+        circle: {
+            width: 140,
+            height: 140,
+            borderWidth: 15,
+            borderRadius: 70
+        },
+        pressButton: {
+            width: 119,
+            height: 48,
+            borderWidth: 1,
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#E3C000',
+        },
+        pressText: {
+            fontSize: 20,
+            fontWeight: '700',
+            lineHeight: 23.44,
+        }
+    });
+
+const Screen2 = function() {
+    return (
+        <View style={{flex: 1, backgroundColor: 'cyan'}}>
+            <LinearGradient colors={['#C7F4F6', '#D1F4F6', '#E5F4F5', '#00CCF9']} style={{flex: 1}}>
+            <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
+                <Text style={style.circle}></Text>
+            </View>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{textAlign: 'center', fontSize: 25, lineHeight: 29, fontWeight: '700'}}>GROW{'\n'}YOUR BUSINESS</Text>
+            </View>
+            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{textAlign: 'center', fontSize: 15, lineHeight: 18}}>We will help you to grow your business using{'\n'}online server</Text>
+            </View>
+            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-evenly', alignItems: 'center'}}>
+                <Pressable style={style.pressButton}>
+                    <Text style={style.pressText}>LOGIN</Text>
+                </Pressable>
+                <Pressable style={style.pressButton}>
+                    <Text style={style.pressText}>SIGN UP</Text>
+                </Pressable>
+            </View>
+            <View style={{flex: 1, alignItems: 'center'}}><Text style={style.pressText}>HOW WE WORK?</Text></View>
+            </LinearGradient>
+        </View>
+    );
+}
+
+export default Screen2;
